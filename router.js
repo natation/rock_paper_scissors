f0ee1bfd4fe8cfe191cf0dd7ff42577a5b574265
Victor <a7lexis@gmail.com>
2015-11-12 12:11:11 -0800

    Consolidated Nav into ApplicationLayout template

--- router.js
+++ router.js
@@ -1,6 +1,27 @@
+Iron.utils.debug = true;
+
 Router.route('/', function () {
+  layoutOptions = {fullNav: true};
+  setLayout.call(this, layoutOptions);
   this.render('Home');
 });
 
-Router.route('/player1');
-Router.route('/player2');
+Router.route('/player1', function () {
+  layoutOptions = {fullNav: false};
+  setLayout.call(this, layoutOptions);
+  this.render('player1');
+});
+
+Router.route('/player2', function () {
+  layoutOptions = {fullNav: false};
+  setLayout.call(this, layoutOptions);
+  this.render('player2');
+});
+
+function setLayout (opts) {
+  this.layout('ApplicationLayout', {
+    data: {
+      fullNav: opts.fullNav
+    }
+  });
+}
